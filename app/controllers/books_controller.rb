8766ce485095d02f76906d40183a44853a69ae5a
EC2 Default User <ec2-user@ip-172-31-44-54.ec2.internal>
2022-05-18 11:45:55 +0000

    [finish]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,37 @@
 class BooksController < ApplicationController
-  def new
-  end
+
 
   def index
+    @user =User.find(current_user.id)
+    @book =Book.new
+    @books =Book.all
+  end
+
+  def create
+    @book =Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @user =User.find(params[:id])
+    @book =Book.new
+    @books =@user.books
   end
 
   def edit
+  end
+
+  def destroy
+    @book =Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:body, :title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
   
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path =Rails.root.join('app/assets/images/noimage-760x460.png')
-      profile_image.attached(io:File.open(file_path), filename:'default-image.jpg', content_type:'image/jpeg')
+      file_path =Rails.root.join('app/assets/images/noimage-760x460.jpg')
+      profile_image.attach(io:File.open(file_path), filename:'default-image.jpg', content_type:'image/jpeg')
     end
     profile_image.variant(resize_to_limit:[width, height]).processed
   end 
